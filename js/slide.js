2a20b4695b7e4704fa6026ef95ff051fe57a2028
azizkone <azizkaramoko.kone@ynov.com>
2019-02-27 22:14:57 +0100

    commit basic

---
+++ js/slide.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+				jQuery(document).ready(function() {
+					   jQuery('.tp-banner').revolution(
+						{
+							delay:9000,
+							startwidth:1170,
+							startheight:500,
+							hideThumbs:10
+						});
+				});
+			</script>
